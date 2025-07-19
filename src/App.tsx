5bdb58023e55b5ac1fadfd156fd528b20457fa42
Alex5683 <137443349+Alex5683@users.noreply.github.com>
2025-07-19 14:01:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BlogPage from './pages/BlogPage';
 import BlogDetailPage from './pages/BlogDetailPage';
+import AboutUsPage from './pages/AboutUs';
+
 function App() {
   return (
     <div className="min-h-screen bg-white">
@@ -10,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/blog/:slug" element={<BlogDetailPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
       </Routes>
     </div>
   );
