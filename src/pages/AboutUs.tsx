b34a93db57cc8f280c3789987a58f8919b8087d5
Alven5683 <Alvensmith1998@gmail.com>
2025-11-08 11:31:33 +0530

    Update AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -51,6 +51,24 @@ const AboutUsPage = () => {
               ))}
             </ul>
           </div>
+
+          {/* Personal Details */}
+          <div className="max-w-2xl mx-auto bg-white p-8 rounded-xl shadow-xl hover:shadow-2xl transform hover:scale-[1.01] transition duration-300 mt-10">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">Contact Information</h2>
+            <div className="flex flex-row items-center justify-between space-x-8">
+              <div className="flex-1 space-y-2 text-left">
+                <p className="text-gray-800 text-base font-medium"><strong>Name:</strong> Akshay Sankhat</p>
+                <p className="text-gray-800 text-base font-medium"><strong>Email:</strong> akshaysankhat@gmail.com</p>
+                <p className="text-gray-800 text-base font-medium"><strong>Phone:</strong> 9016244785</p>
+                <p className="text-gray-800 text-base font-medium"><strong>Address:</strong> H 302 Laxmivilla 2, New Naroda, Ahmedabad, 382340</p>
+              </div>
+              <img
+                src="https://scontent.famd21-2.fna.fbcdn.net/v/t1.6435-9/70026148_2310429465885111_6015257996202868736_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=Q1z28H60RvEQ7kNvwG6ntCW&_nc_oc=AdlS-7rDfo8LoBzMkpF_U3_DYS6T240syqU6orGWkjF1jZQOnOTZfDfVOXW30-5d11IjH821N0kP8Wv28n2LNQyW&_nc_zt=23&_nc_ht=scontent.famd21-2.fna&_nc_gid=xTf9scpi6gjN7du5P9JgdA&oh=00_Afggzjbt_SM7pA1sQVakI6g4_PNsNy_OTkFTx1pIDS8eNg&oe=69364640"
+                alt="Profile Picture"
+                className="w-32 h-32 rounded-full object-cover shadow-lg flex-shrink-0"
+              />
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
